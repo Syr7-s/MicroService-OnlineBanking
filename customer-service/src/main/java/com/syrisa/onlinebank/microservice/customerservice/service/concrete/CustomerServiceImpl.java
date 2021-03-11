9fcccc7ddd1c2d9fc2e3c2ba37338f451ee4fe54
Syr7-s <isasayar7@github.com>
2021-03-11 21:05:52 +0300

    create method was edited in the CustomerServiceImpl.

--- customer-service/src/main/java/com/syrisa/onlinebank/microservice/customerservice/service/concrete/CustomerServiceImpl.java
+++ customer-service/src/main/java/com/syrisa/onlinebank/microservice/customerservice/service/concrete/CustomerServiceImpl.java
@@ -2,23 +2,35 @@ package com.syrisa.onlinebank.microservice.customerservice.service.concrete;
 
 
 import com.syrisa.onlinebank.microservice.customerservice.entity.Customer;
+import com.syrisa.onlinebank.microservice.customerservice.repository.AddressRepository;
 import com.syrisa.onlinebank.microservice.customerservice.repository.CustomerRepository;
 import com.syrisa.onlinebank.microservice.customerservice.service.abstrct.CustomerService;
+import com.syrisa.onlinebank.microservice.customerservice.utility.generate.tc.TC;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 @Service
 public class CustomerServiceImpl implements CustomerService<Customer> {
     private final CustomerRepository customerRepository;
+    private final AddressRepository addressRepository;
 
-    public CustomerServiceImpl(CustomerRepository customerRepository) {
+    public CustomerServiceImpl(CustomerRepository customerRepository, AddressRepository addressRepository) {
         this.customerRepository = customerRepository;
+        this.addressRepository = addressRepository;
     }
 
     @Override
     public Customer create(Customer customer) {
-        return null;
+        try {
+            customer.setCustomerTC(TC.generateTC.get());
+            addressRepository.save(customer.getAddress());
+            return customerRepository.save(customer);
+        } catch (Exception exception) {
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, exception.getMessage());
+        }
     }
 
     @Override
