eca28ce8032b1444daa28ef2543bf1eb02a7077b
Syr7-s <isasayar7@github.com>
2021-03-11 21:04:05 +0300

    findAll method was added to the CustomerRepository.

--- customer-service/src/main/java/com/syrisa/onlinebank/microservice/customerservice/repository/CustomerRepository.java
+++ customer-service/src/main/java/com/syrisa/onlinebank/microservice/customerservice/repository/CustomerRepository.java
@@ -1,9 +1,12 @@
 package com.syrisa.onlinebank.microservice.customerservice.repository;
 
 import com.syrisa.onlinebank.microservice.customerservice.entity.Customer;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CustomerRepository extends CrudRepository<Customer,Long> {
+public interface CustomerRepository extends CrudRepository<Customer, Long> {
+    Page<Customer> findAll(Pageable pageable);
 }
