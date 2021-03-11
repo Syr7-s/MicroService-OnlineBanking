4d09d8892bc20436f31bbd9d725b2e762e7c98fb
Syr7-s <isasayar7@github.com>
2021-03-11 20:47:46 +0300

    Attributes were added to the CustomerDto

--- customer-service/src/main/java/com/syrisa/onlinebank/microservice/customerservice/dto/CustomerDto.java
+++ customer-service/src/main/java/com/syrisa/onlinebank/microservice/customerservice/dto/CustomerDto.java
@@ -1,4 +1,37 @@
 package com.syrisa.onlinebank.microservice.customerservice.dto;
 
+import com.syrisa.onlinebank.microservice.customerservice.entity.Address;
+import com.syrisa.onlinebank.microservice.customerservice.entity.Customer;
+import com.syrisa.onlinebank.microservice.customerservice.utility.enums.Gender;
+import lombok.Builder;
+import lombok.Data;
+
+import java.time.LocalDate;
+
+@Data
+@Builder
 public class CustomerDto {
+    private long customerTC;
+    private String customerName;
+    private String customerLastname;
+    private Gender customerGender;
+    private String customerPhone;
+    private String customerEmail;
+    private LocalDate customerBirthDate;
+    private boolean isState;
+    private Address address;
+
+    public Customer toCustomer() {
+        return Customer.builder()
+                .customerTC(this.customerTC)
+                .customerName(this.customerName)
+                .customerLastname(this.customerLastname)
+                .customerGender(this.customerGender)
+                .customerPhone(this.customerPhone)
+                .customerEmail(this.customerEmail)
+                .customerBirthDate(this.customerBirthDate)
+                .isState(this.isState)
+                .address(this.address)
+                .build();
+    }
 }
