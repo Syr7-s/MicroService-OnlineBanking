85eaabae93179f2fb2a7a5a9366ab08d26c36dc4
Syr7-s <isasayar7@github.com>
2021-03-11 21:03:31 +0300

    CustomerService and CustomerServiceImpl was edited.

--- customer-service/src/main/java/com/syrisa/onlinebank/microservice/customerservice/service/abstrct/CustomerService.java
+++ customer-service/src/main/java/com/syrisa/onlinebank/microservice/customerservice/service/abstrct/CustomerService.java
@@ -1,4 +1,16 @@
 package com.syrisa.onlinebank.microservice.customerservice.service.abstrct;
 
-public interface CustomerService {
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+public interface CustomerService<T> {
+    T create(T t);
+
+    T get(long tc);
+
+    T update(T t);
+
+    String delete(long tc);
+
+    Page<T> getCustomers(Pageable pageable);
 }
--- customer-service/src/main/java/com/syrisa/onlinebank/microservice/customerservice/service/concrete/CustomerServiceImpl.java
+++ customer-service/src/main/java/com/syrisa/onlinebank/microservice/customerservice/service/concrete/CustomerServiceImpl.java
@@ -1,7 +1,43 @@
 package com.syrisa.onlinebank.microservice.customerservice.service.concrete;
 
+
+import com.syrisa.onlinebank.microservice.customerservice.entity.Customer;
+import com.syrisa.onlinebank.microservice.customerservice.repository.CustomerRepository;
+import com.syrisa.onlinebank.microservice.customerservice.service.abstrct.CustomerService;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 @Service
-public class CustomerServiceImpl {
+public class CustomerServiceImpl implements CustomerService<Customer> {
+    private final CustomerRepository customerRepository;
+
+    public CustomerServiceImpl(CustomerRepository customerRepository) {
+        this.customerRepository = customerRepository;
+    }
+
+    @Override
+    public Customer create(Customer customer) {
+        return null;
+    }
+
+    @Override
+    public Customer get(long tc) {
+        return null;
+    }
+
+    @Override
+    public Customer update(Customer customer) {
+        return null;
+    }
+
+    @Override
+    public String delete(long tc) {
+        return null;
+    }
+
+    @Override
+    public Page<Customer> getCustomers(Pageable pageable) {
+        return null;
+    }
 }
