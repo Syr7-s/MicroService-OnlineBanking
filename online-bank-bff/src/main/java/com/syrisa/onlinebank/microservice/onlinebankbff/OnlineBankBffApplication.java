263b8c23577978cb0623d96bd9bb58f957e1eef4
Syr7-s <isasayar7@github.com>
2021-03-12 16:21:45 +0300

    @EnableFeignClients annotation was added to the OnlineBankBffApplication

--- online-bank-bff/src/main/java/com/syrisa/onlinebank/microservice/onlinebankbff/OnlineBankBffApplication.java
+++ online-bank-bff/src/main/java/com/syrisa/onlinebank/microservice/onlinebankbff/OnlineBankBffApplication.java
@@ -2,7 +2,9 @@ package com.syrisa.onlinebank.microservice.onlinebankbff;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableFeignClients
 @SpringBootApplication
 public class OnlineBankBffApplication {
 
