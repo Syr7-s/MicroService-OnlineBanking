365239635a967f006fe377241f891c8d2eeef4cc
Syr7-s <isasayar7@github.com>
2021-03-12 16:19:56 +0300

    initial commit

---
+++ online-bank-bff/src/main/java/com/syrisa/onlinebank/microservice/onlinebankbff/OnlineBankBffApplication.java
@@ -0,0 +1,13 @@
+package com.syrisa.onlinebank.microservice.onlinebankbff;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineBankBffApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnlineBankBffApplication.class, args);
+    }
+
+}
