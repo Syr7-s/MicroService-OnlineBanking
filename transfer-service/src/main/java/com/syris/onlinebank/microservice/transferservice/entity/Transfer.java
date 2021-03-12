95db511f1179c9f64874e8977cb0ff320bf2b6ae
Syr7-s <isasayar7@github.com>
2021-03-12 17:12:03 +0300

    attributes were added to the Transfer

--- transfer-service/src/main/java/com/syris/onlinebank/microservice/transferservice/entity/Transfer.java
+++ transfer-service/src/main/java/com/syris/onlinebank/microservice/transferservice/entity/Transfer.java
@@ -1,4 +1,13 @@
 package com.syris.onlinebank.microservice.transferservice.entity;
 
+import lombok.Data;
+
+import java.time.LocalDate;
+import java.util.Map;
+
+@Data
 public class Transfer {
+    private Map<String, Double> rates;
+    private String base;
+    private LocalDate date;
 }
