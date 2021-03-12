9a445a44e54046929302c5fe75b89fe2d0e14a31
Syr7-s <isasayar7@github.com>
2021-03-12 17:18:20 +0300

    TransferServiceClient was edited.Also Currency enum was created.

--- online-bank-bff/src/main/java/com/syrisa/onlinebank/microservice/onlinebankbff/client/TransferServiceClient.java
+++ online-bank-bff/src/main/java/com/syrisa/onlinebank/microservice/onlinebankbff/client/TransferServiceClient.java
@@ -1,4 +1,17 @@
 package com.syrisa.onlinebank.microservice.onlinebankbff.client;
 
+import com.syrisa.onlinebank.microservice.onlinebankbff.utility.enums.currency.Currency;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.validation.constraints.NotNull;
+
+@FeignClient("transfer-service")
 public interface TransferServiceClient {
+    @GetMapping("/api/v1/exchange/{currency}/transfer/{convertCurrency}")
+    double getConvertMoney(@NotNull @PathVariable("currency") Currency currency,
+                           @NotNull @PathVariable("convertCurrency") Currency convertCurrency,
+                           @RequestParam("money") int money);
 }
