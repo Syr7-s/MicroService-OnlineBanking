f10446b6c59c353426d578af42564ded70fecb82
Syr7-s <isasayar7@github.com>
2021-03-13 13:47:17 +0300

    Account class was created.

---
+++ account-service/src/main/java/com/syrisa/onlinebank/microservice/accountservice/utility/generate/account/Account.java
@@ -0,0 +1,19 @@
+package com.syrisa.onlinebank.microservice.accountservice.utility.generate.account;
+
+import java.util.Random;
+import java.util.function.Supplier;
+
+public class Account {
+    private Account() {
+
+    }
+
+    public static final Supplier<String> generateAccount = () -> {
+        StringBuilder accountNumber = new StringBuilder();
+        accountNumber.append("8500");
+        for (int i = 0; i < 12; i++) {
+            accountNumber.append(new Random().nextInt(10));
+        }
+        return accountNumber.toString();
+    };
+}
