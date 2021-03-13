3cef11272691d1b497a95990421dd8fd13613797
Syr7-s <isasayar7@github.com>
2021-03-13 12:34:04 +0300

    initial commit

---
+++ account-service/src/test/java/com/syrisa/onlinebank/microservice/accountservice/AccountServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.syrisa.onlinebank.microservice.accountservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
