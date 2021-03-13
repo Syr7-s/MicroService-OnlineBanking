4abe205b68468885fd2620e34614699a105d8472
Syr7-s <isasayar7@github.com>
2021-03-13 13:19:42 +0300

    SavingsAccountServiceImpl was edited.

--- account-service/src/main/java/com/syrisa/onlinebank/microservice/accountservice/service/impl/SavingsAccountServiceImpl.java
+++ account-service/src/main/java/com/syrisa/onlinebank/microservice/accountservice/service/impl/SavingsAccountServiceImpl.java
@@ -1,40 +1,70 @@
 package com.syrisa.onlinebank.microservice.accountservice.service.impl;
 
 import com.syrisa.onlinebank.microservice.accountservice.entity.SavingsAccount;
+import com.syrisa.onlinebank.microservice.accountservice.repository.SavingsAccountRepository;
 import com.syrisa.onlinebank.microservice.accountservice.service.SavingsAccountService;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
 @Service
 public class SavingsAccountServiceImpl implements SavingsAccountService {
+    private final SavingsAccountRepository savingsAccountRepository;
+
+    public SavingsAccountServiceImpl(SavingsAccountRepository savingsAccountRepository) {
+        this.savingsAccountRepository = savingsAccountRepository;
+    }
+
     @Override
     public SavingsAccount create(SavingsAccount savingsAccount) {
-        return null;
+        return savingsAccountRepository.save(savingsAccount);
     }
 
     @Override
     public SavingsAccount update(SavingsAccount savingsAccount) {
-        return null;
+        if (get(savingsAccount.getAccountNumber()) != null) {
+            return savingsAccountRepository.save(savingsAccount);
+        } else {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Customer have not account");
+        }
     }
 
     @Override
     public SavingsAccount get(long accountNumber) {
-        return null;
+        return savingsAccountRepository.findById(accountNumber)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Account not found"));
     }
 
     @Override
     public SavingsAccount getAccountByIBAN(String accountIBAN) {
-        return null;
+        SavingsAccount savingsAccount = savingsAccountRepository.getSavingsAccountByAccountIban(accountIBAN);
+        if (savingsAccount != null) {
+            return savingsAccount;
+        } else {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Account not found");
+        }
     }
 
     @Override
     public List<SavingsAccount> getAccountByCustomers(long customerTC) {
-        return null;
+        List<SavingsAccount> savingsAccounts = savingsAccountRepository.getSavingsAccountsByCustomerTC(customerTC);
+        if (!savingsAccounts.isEmpty()) {
+            return savingsAccounts;
+        } else {
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Customer have not accounts");
+        }
     }
 
     @Override
     public String delete(long accountNumber) {
-        return null;
+        SavingsAccount savingsAccount = get(accountNumber);
+        if (savingsAccount.getAccountBalance() == 0) {
+            savingsAccountRepository.delete(savingsAccount);
+            return savingsAccount.getAccountNumber() + " number account was deleted.";
+        } else {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Have money in your account");
+        }
     }
 }
