9f018a31f6fa1811cf913d853d0deb42b3cbec1f
Syr7-s <isasayar7@github.com>
2021-03-13 12:57:49 +0300

    getSavingsAccountByAccountIban method was added.

--- account-service/src/main/java/com/syrisa/onlinebank/microservice/accountservice/repository/SavingsAccountRepository.java
+++ account-service/src/main/java/com/syrisa/onlinebank/microservice/accountservice/repository/SavingsAccountRepository.java
@@ -1,7 +1,10 @@
 package com.syrisa.onlinebank.microservice.accountservice.repository;
 
+import com.syrisa.onlinebank.microservice.accountservice.entity.SavingsAccount;
+import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface SavingsAccountRepository {
+public interface SavingsAccountRepository extends MongoRepository<SavingsAccount, Long> {
+    SavingsAccount getSavingsAccountByAccountIban(String iban);
 }
