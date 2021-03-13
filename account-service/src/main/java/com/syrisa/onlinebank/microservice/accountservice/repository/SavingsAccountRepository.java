c82156146772cb5fd95426583c225d407b8326c3
Syr7-s <isasayar7@github.com>
2021-03-13 12:56:45 +0300

    @Repository annotation was added.

--- account-service/src/main/java/com/syrisa/onlinebank/microservice/accountservice/repository/SavingsAccountRepository.java
+++ account-service/src/main/java/com/syrisa/onlinebank/microservice/accountservice/repository/SavingsAccountRepository.java
@@ -1,4 +1,7 @@
 package com.syrisa.onlinebank.microservice.accountservice.repository;
 
+import org.springframework.stereotype.Repository;
+
+@Repository
 public interface SavingsAccountRepository {
 }
