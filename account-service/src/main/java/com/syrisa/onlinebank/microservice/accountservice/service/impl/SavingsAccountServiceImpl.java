6e36909e0e97cf841a6f87a6c5052e3129d19933
Syr7-s <isasayar7@github.com>
2021-03-13 12:54:23 +0300

    SavingsAccountServiceImpl was edited.SavingsAccountService was implemented.

--- account-service/src/main/java/com/syrisa/onlinebank/microservice/accountservice/service/impl/SavingsAccountServiceImpl.java
+++ account-service/src/main/java/com/syrisa/onlinebank/microservice/accountservice/service/impl/SavingsAccountServiceImpl.java
@@ -1,4 +1,33 @@
 package com.syrisa.onlinebank.microservice.accountservice.service.impl;
 
-public class SavingsAccountServiceImpl {
+import com.syrisa.onlinebank.microservice.accountservice.entity.SavingsAccount;
+import com.syrisa.onlinebank.microservice.accountservice.service.SavingsAccountService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class SavingsAccountServiceImpl implements SavingsAccountService {
+    @Override
+    public SavingsAccount create(SavingsAccount savingsAccount) {
+        return null;
+    }
+
+    @Override
+    public SavingsAccount update(SavingsAccount savingsAccount) {
+        return null;
+    }
+
+    @Override
+    public SavingsAccount get(long accountNumber) {
+        return null;
+    }
+
+    @Override
+    public SavingsAccount getAccountByIBAN(String accountIBAN) {
+        return null;
+    }
+
+    @Override
+    public String delete(long accountNumber) {
+        return null;
+    }
 }
