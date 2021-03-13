b067c78018ff6da2fc724b46c589dc925622c476
Syr7-s <isasayar7@github.com>
2021-03-13 14:01:45 +0300

    AccountDto was edited.

--- account-service/src/main/java/com/syrisa/onlinebank/microservice/accountservice/dto/AccountDto.java
+++ account-service/src/main/java/com/syrisa/onlinebank/microservice/accountservice/dto/AccountDto.java
@@ -1,8 +1,31 @@
 package com.syrisa.onlinebank.microservice.accountservice.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.syrisa.onlinebank.microservice.accountservice.entity.Account;
+import com.syrisa.onlinebank.microservice.accountservice.utility.currency.Currency;
+import lombok.Builder;
 import lombok.Data;
 
-@Data
-public class AccountDto {
+import java.time.LocalDate;
 
+@Data
+@Builder
+public class AccountDto {
+    private long accountNumber;
+    private String accountIban;
+    private int accountBalance;
+    private Currency accountCurrency;
+    private LocalDate accountCreationDate;
+    private long customerTC;
+
+    public Account toAccount() {
+        return Account.builder()
+                .accountNumber(this.accountNumber)
+                .accountIban(this.accountIban)
+                .accountBalance(this.accountBalance)
+                .accountCurrency(this.accountCurrency)
+                .accountCreationDate(this.accountCreationDate)
+                .customerTC(this.customerTC)
+                .build();
+    }
 }
