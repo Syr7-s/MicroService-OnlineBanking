c1f2dc9779942474d42e44ad53368197d8dd634e
Syr7-s <isasayar7@github.com>
2021-03-14 12:50:19 +0300

    attributes were added to the DemandDepositAccountDto

--- onlinebank-app-bff/src/main/java/com/syrisa/onlinebank/microservice/onlinebankbff/dto/DemandDepositAccountDto.java
+++ onlinebank-app-bff/src/main/java/com/syrisa/onlinebank/microservice/onlinebankbff/dto/DemandDepositAccountDto.java
@@ -1,4 +1,31 @@
 package com.syrisa.onlinebank.microservice.onlinebankbff.dto;
 
+import com.syrisa.onlinebank.microservice.onlinebankbff.entity.DemandDepositAccount;
+import com.syrisa.onlinebank.microservice.onlinebankbff.utility.enums.currency.Currency;
+import lombok.Builder;
+import lombok.Data;
+
+import java.time.LocalDate;
+
+@Data
+@Builder
 public class DemandDepositAccountDto {
+    private long accountNumber;
+    private String accountIban;
+    private int accountBalance;
+    private Currency accountCurrency;
+    private LocalDate accountCreationDate;
+    private long customerTC;
+
+    public DemandDepositAccount toDemandDepositAccount(){
+        return DemandDepositAccount.builder()
+                .accountNumber(this.accountNumber)
+                .accountIban(this.accountIban)
+                .accountBalance(this.accountBalance)
+                .accountCurrency(this.accountCurrency)
+                .accountCreationDate(this.accountCreationDate)
+                .customerTC(this.customerTC)
+                .build();
+    }
+
 }
