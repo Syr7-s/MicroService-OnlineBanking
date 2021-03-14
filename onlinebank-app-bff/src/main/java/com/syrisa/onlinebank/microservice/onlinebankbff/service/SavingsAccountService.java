417ee72c2bdd7bb9cccdb6b22beb7eefeac9cbe7
Syr7-s <isasayar7@github.com>
2021-03-14 13:02:58 +0300

    SavingsAccountService was created.

---
+++ onlinebank-app-bff/src/main/java/com/syrisa/onlinebank/microservice/onlinebankbff/service/SavingsAccountService.java
@@ -0,0 +1,6 @@
+package com.syrisa.onlinebank.microservice.onlinebankbff.service;
+
+import com.syrisa.onlinebank.microservice.onlinebankbff.entity.SavingsAccount;
+
+public interface SavingsAccountService extends AccountService<SavingsAccount> {
+}
