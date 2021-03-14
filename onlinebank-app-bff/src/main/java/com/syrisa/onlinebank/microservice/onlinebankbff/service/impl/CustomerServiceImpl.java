25a1057a38a82ce14eb6da7e846eb2a835a4e36b
Syr7-s <isasayar7@github.com>
2021-03-14 14:01:01 +0300

    CustomerServiceImpl.java was edited.

--- onlinebank-app-bff/src/main/java/com/syrisa/onlinebank/microservice/onlinebankbff/service/impl/CustomerServiceImpl.java
+++ onlinebank-app-bff/src/main/java/com/syrisa/onlinebank/microservice/onlinebankbff/service/impl/CustomerServiceImpl.java
@@ -1,22 +1,37 @@
 package com.syrisa.onlinebank.microservice.onlinebankbff.service.impl;
 
 import com.syrisa.onlinebank.microservice.onlinebankbff.client.CustomerServiceClient;
+import com.syrisa.onlinebank.microservice.onlinebankbff.client.DemandDepositAccountServiceClient;
+import com.syrisa.onlinebank.microservice.onlinebankbff.client.SavingsAccountServiceClient;
 import com.syrisa.onlinebank.microservice.onlinebankbff.dto.CustomerDto;
+import com.syrisa.onlinebank.microservice.onlinebankbff.dto.DemandDepositAccountDto;
+import com.syrisa.onlinebank.microservice.onlinebankbff.dto.SavingsAccountDto;
 import com.syrisa.onlinebank.microservice.onlinebankbff.entity.Customer;
 import com.syrisa.onlinebank.microservice.onlinebankbff.service.CustomerService;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Service
 public class CustomerServiceImpl implements CustomerService<Customer> {
     private final CustomerServiceClient customerServiceClient;
+    private final DemandDepositAccountServiceClient demandDepositAccountServiceClient;
+    private final SavingsAccountServiceClient savingsAccountServiceClient;
 
-    public CustomerServiceImpl(CustomerServiceClient customerServiceClient) {
+    public CustomerServiceImpl(CustomerServiceClient customerServiceClient, DemandDepositAccountServiceClient demandDepositAccountServiceClient, SavingsAccountServiceClient savingsAccountServiceClient) {
         this.customerServiceClient = customerServiceClient;
+        this.demandDepositAccountServiceClient = demandDepositAccountServiceClient;
+        this.savingsAccountServiceClient = savingsAccountServiceClient;
     }
 
     @Override
     public Customer get(long tc) {
         CustomerDto customerDto = customerServiceClient.get(tc);
+        List<DemandDepositAccountDto> demandDepositAccounts = demandDepositAccountServiceClient.getDemandDepositAccountByCustomerTC(tc);
+        List<SavingsAccountDto> savingsAccounts = savingsAccountServiceClient.getDemandDepositAccountByCustomerTC(tc);
+        customerDto.setDemandDepositAccounts(demandDepositAccounts.stream().map(DemandDepositAccountDto::toDemandDepositAccount).collect(Collectors.toList()));
+        customerDto.setSavingsAccounts(savingsAccounts.stream().map(SavingsAccountDto::toSavingsAccount).collect(Collectors.toList()));
         return customerDto.toCustomer();
     }
 }
