bf2fb255a4af44edf4b542cb8aa7133a1d02e850
Syr7-s <isasayar7@github.com>
2021-03-14 13:50:06 +0300

    DemandDepositAccountServiceImpl was edited.And DemandDepositAccountServiceImpl was created.

--- onlinebank-app-bff/src/main/java/com/syrisa/onlinebank/microservice/onlinebankbff/client/DemandDepositAccountServiceClient.java
+++ onlinebank-app-bff/src/main/java/com/syrisa/onlinebank/microservice/onlinebankbff/client/DemandDepositAccountServiceClient.java
@@ -1,8 +1,17 @@
 package com.syrisa.onlinebank.microservice.onlinebankbff.client;
 
+import com.syrisa.onlinebank.microservice.onlinebankbff.dto.DemandDepositAccountDto;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.List;
 
 @FeignClient("account-service")
 public interface DemandDepositAccountServiceClient {
+    @GetMapping("/demands/{customerTC}")
+    List<DemandDepositAccountDto> getDemandDepositAccountByCustomerTC(@PathVariable("customerTC") long customerTC);
 
+    @GetMapping("/demand/iban/{accountIban}")
+    DemandDepositAccountDto getDemandDepositAccountByAccountIban(@PathVariable("accountIban") String accountIban);
 }
---
+++ onlinebank-app-bff/src/main/java/com/syrisa/onlinebank/microservice/onlinebankbff/service/impl/DemandDepositAccountServiceImpl.java
@@ -0,0 +1,11 @@
+package com.syrisa.onlinebank.microservice.onlinebankbff.service.impl;
+
+import com.syrisa.onlinebank.microservice.onlinebankbff.entity.DemandDepositAccount;
+import com.syrisa.onlinebank.microservice.onlinebankbff.service.DemandDepositAccountService;
+
+public class DemandDepositAccountServiceImpl implements DemandDepositAccountService {
+    @Override
+    public DemandDepositAccount getAccountByIban(String accountIban) {
+        return null;
+    }
+}
