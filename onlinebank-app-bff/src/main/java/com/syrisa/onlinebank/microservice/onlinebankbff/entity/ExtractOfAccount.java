0725895715282fdfedb4fcadee93b32347a94563
Syr7-s <isasayar7@github.com>
2021-03-14 12:48:43 +0300

    attributes were added to the ExtractOfAccount

--- onlinebank-app-bff/src/main/java/com/syrisa/onlinebank/microservice/onlinebankbff/entity/ExtractOfAccount.java
+++ onlinebank-app-bff/src/main/java/com/syrisa/onlinebank/microservice/onlinebankbff/entity/ExtractOfAccount.java
@@ -1,4 +1,20 @@
 package com.syrisa.onlinebank.microservice.onlinebankbff.entity;
 
+import lombok.*;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.UUID;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class ExtractOfAccount {
+    private UUID id;
+    private long accountNumber;
+    private int money;
+    private LocalDate date;
+    private LocalTime time;
 }
