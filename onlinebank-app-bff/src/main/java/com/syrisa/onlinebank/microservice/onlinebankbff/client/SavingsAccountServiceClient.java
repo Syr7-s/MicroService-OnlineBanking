63e1bc7ea25744eb8823c771a4b2fc38b79bcb38
Syr7-s <isasayar7@github.com>
2021-03-14 13:53:33 +0300

    SavingsAccountServiceClient was created.

--- onlinebank-app-bff/src/main/java/com/syrisa/onlinebank/microservice/onlinebankbff/client/SavingsAccountServiceClient.java
+++ onlinebank-app-bff/src/main/java/com/syrisa/onlinebank/microservice/onlinebankbff/client/SavingsAccountServiceClient.java
@@ -1,7 +1,17 @@
 package com.syrisa.onlinebank.microservice.onlinebankbff.client;
 
+import com.syrisa.onlinebank.microservice.onlinebankbff.dto.SavingsAccountDto;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.List;
 
 @FeignClient("account-service")
 public interface SavingsAccountServiceClient {
+    @GetMapping("/savings/iban/{accountIban}")
+    SavingsAccountDto getDemandDepositAccountByAccountIban(@PathVariable("accountIban") String accountIban);
+
+    @GetMapping("/savings/{customerTC}")
+    List<SavingsAccountDto> getDemandDepositAccountByCustomerTC(@PathVariable("customerTC") long customerTC);
 }
