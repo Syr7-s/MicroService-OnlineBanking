78f578dfd4aea7af7d04f0ceb139e0595533fce0
Syr7-s <isasayar7@github.com>
2021-03-14 13:00:41 +0300

    DemandDepositAccountService was created.

---
+++ onlinebank-app-bff/src/main/java/com/syrisa/onlinebank/microservice/onlinebankbff/service/impl/DemandDepositAccountService.java
@@ -0,0 +1,7 @@
+package com.syrisa.onlinebank.microservice.onlinebankbff.service.impl;
+
+import com.syrisa.onlinebank.microservice.onlinebankbff.entity.DemandDepositAccount;
+import com.syrisa.onlinebank.microservice.onlinebankbff.service.AccountService;
+
+public interface DemandDepositAccountService extends AccountService<DemandDepositAccount> {
+}
