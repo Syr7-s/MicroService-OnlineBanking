8c13d19d4a68d5c1acafb41f16ed85d98c40c056
Syr7-s <isasayar7@github.com>
2021-03-14 13:52:21 +0300

    DemandDepositAccountServiceImpl was edited.

--- onlinebank-app-bff/src/main/java/com/syrisa/onlinebank/microservice/onlinebankbff/service/impl/DemandDepositAccountServiceImpl.java
+++ onlinebank-app-bff/src/main/java/com/syrisa/onlinebank/microservice/onlinebankbff/service/impl/DemandDepositAccountServiceImpl.java
@@ -1,11 +1,25 @@
 package com.syrisa.onlinebank.microservice.onlinebankbff.service.impl;
 
+import com.syrisa.onlinebank.microservice.onlinebankbff.client.DemandDepositAccountServiceClient;
 import com.syrisa.onlinebank.microservice.onlinebankbff.entity.DemandDepositAccount;
 import com.syrisa.onlinebank.microservice.onlinebankbff.service.DemandDepositAccountService;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.server.ResponseStatusException;
 
 public class DemandDepositAccountServiceImpl implements DemandDepositAccountService {
+    private final DemandDepositAccountServiceClient demandDepositAccountServiceClient;
+
+    public DemandDepositAccountServiceImpl(DemandDepositAccountServiceClient demandDepositAccountServiceClient) {
+        this.demandDepositAccountServiceClient = demandDepositAccountServiceClient;
+    }
+
     @Override
     public DemandDepositAccount getAccountByIban(String accountIban) {
-        return null;
+        try {
+            return demandDepositAccountServiceClient.getDemandDepositAccountByAccountIban(accountIban).toDemandDepositAccount();
+        } catch (Exception exception) {
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, exception.getMessage());
+        }
+
     }
 }
