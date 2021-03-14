84daf915a16f59b0571f83a2ff08ae1980f3f674
Syr7-s <isasayar7@github.com>
2021-03-14 12:45:55 +0300

    attributes were added to the DemandDepositAccount

--- onlinebank-app-bff/src/main/java/com/syrisa/onlinebank/microservice/onlinebankbff/entity/DemandDepositAccount.java
+++ onlinebank-app-bff/src/main/java/com/syrisa/onlinebank/microservice/onlinebankbff/entity/DemandDepositAccount.java
@@ -1,4 +1,21 @@
 package com.syrisa.onlinebank.microservice.onlinebankbff.entity;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.syrisa.onlinebank.microservice.onlinebankbff.utility.enums.currency.Currency;
+import lombok.*;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class DemandDepositAccount {
+    private long accountNumber;
+    private String accountIban;
+    private int accountBalance;
+    private Currency accountCurrency;
+    private LocalDate accountCreationDate;
+    private long customerTC;
 }
