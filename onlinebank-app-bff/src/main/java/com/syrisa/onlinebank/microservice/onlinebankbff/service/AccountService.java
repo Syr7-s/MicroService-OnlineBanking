8b942ac636b46ac77830da2643c65d1ec493f199
Syr7-s <isasayar7@github.com>
2021-03-14 13:05:16 +0300

    AccountService was edited

--- onlinebank-app-bff/src/main/java/com/syrisa/onlinebank/microservice/onlinebankbff/service/AccountService.java
+++ onlinebank-app-bff/src/main/java/com/syrisa/onlinebank/microservice/onlinebankbff/service/AccountService.java
@@ -1,5 +1,5 @@
 package com.syrisa.onlinebank.microservice.onlinebankbff.service;
 
-public interface AccountService <T>{
+public interface AccountService<T> {
     T getAccountByIban(String accountIban);
 }
