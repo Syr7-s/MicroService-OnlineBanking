c506e0869b6a2bbbd8a23da7a15700dc8f883686
Syr7-s <isasayar7@github.com>
2021-03-16 15:53:45 +0300

    toExchange method was created ExchangeDto

--- account-service/src/main/java/com/syrisa/onlinebank/microservice/accountservice/dto/ExchangeDto.java
+++ account-service/src/main/java/com/syrisa/onlinebank/microservice/accountservice/dto/ExchangeDto.java
@@ -1,5 +1,6 @@
 package com.syrisa.onlinebank.microservice.accountservice.dto;
 
+import com.syrisa.onlinebank.microservice.accountservice.entity.impl.Exchange;
 import lombok.Builder;
 import lombok.Data;
 
@@ -16,4 +17,16 @@ public class ExchangeDto {
     private String processType;
     private LocalTime time;
     private LocalDate date;
+
+    public Exchange toExchange(){
+        return Exchange.builder()
+                .fromAccountIban(this.fromAccountIban)
+                .toAccountIban(this.toAccountIban)
+                .depositMoney(this.depositMoney)
+                .receiveMoney(this.receiveMoney)
+                .processType(this.processType)
+                .time(this.time)
+                .date(this.date)
+                .build();
+    }
 }
