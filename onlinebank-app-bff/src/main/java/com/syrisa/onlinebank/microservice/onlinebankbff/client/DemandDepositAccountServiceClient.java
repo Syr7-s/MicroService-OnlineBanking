5a916a51552e5be1f5cbdc1926bdca952b6f67a1
Syr7-s <isasayar7@github.com>
2021-03-16 12:51:50 +0300

    depositMoneyDemand method was added to the DemandDepositAccountServiceClient

--- onlinebank-app-bff/src/main/java/com/syrisa/onlinebank/microservice/onlinebankbff/client/DemandDepositAccountServiceClient.java
+++ onlinebank-app-bff/src/main/java/com/syrisa/onlinebank/microservice/onlinebankbff/client/DemandDepositAccountServiceClient.java
@@ -1,9 +1,10 @@
 package com.syrisa.onlinebank.microservice.onlinebankbff.client;
 
 import com.syrisa.onlinebank.microservice.onlinebankbff.dto.DemandDepositAccountDto;
+import com.syrisa.onlinebank.microservice.onlinebankbff.dto.ExtractOfAccountDto;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -14,4 +15,7 @@ public interface DemandDepositAccountServiceClient {
 
     @GetMapping("/api/v1/demand/iban/{accountIban}")
     DemandDepositAccountDto getDemandDepositAccountByAccountIban(@PathVariable("accountIban") String accountIban);
+
+    @PostMapping("/api/v1/demand/depositMoney")
+    DemandDepositAccountDto depositMoneyDemand(@RequestBody ExtractOfAccountDto extractOfAccountDto);
 }
