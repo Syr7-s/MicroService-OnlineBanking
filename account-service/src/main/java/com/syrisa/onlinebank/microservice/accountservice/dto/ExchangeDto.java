0c6d0d0e09ca6ec88d281a2b80410e20bffcfbdf
Syr7-s <isasayar7@github.com>
2021-03-16 15:51:32 +0300

    attributes were added to the ExchangeDto

--- account-service/src/main/java/com/syrisa/onlinebank/microservice/accountservice/dto/ExchangeDto.java
+++ account-service/src/main/java/com/syrisa/onlinebank/microservice/accountservice/dto/ExchangeDto.java
@@ -3,7 +3,17 @@ package com.syrisa.onlinebank.microservice.accountservice.dto;
 import lombok.Builder;
 import lombok.Data;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
+
 @Data
 @Builder
 public class ExchangeDto {
+    private long fromAccountIban;
+    private long toAccountIban;
+    private int depositMoney;
+    private int receiveMoney;
+    private String processType;
+    private LocalTime time;
+    private LocalDate date;
 }
