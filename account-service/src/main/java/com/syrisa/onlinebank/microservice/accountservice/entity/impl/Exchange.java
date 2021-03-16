07d89ea40524e80c7895024cdd608fa8657c898d
Syr7-s <isasayar7@github.com>
2021-03-16 15:49:43 +0300

    Exchange renamed from Transfer

--- account-service/src/main/java/com/syrisa/onlinebank/microservice/accountservice/entity/impl/Exchange.java
+++ account-service/src/main/java/com/syrisa/onlinebank/microservice/accountservice/entity/impl/Exchange.java
@@ -14,7 +14,7 @@ import java.time.LocalTime;
 @AllArgsConstructor
 @Builder
 @Document
-public class Transfer {
+public class Exchange {
     @MongoId
     private long fromAccountIban;
     private long toAccountIban;
