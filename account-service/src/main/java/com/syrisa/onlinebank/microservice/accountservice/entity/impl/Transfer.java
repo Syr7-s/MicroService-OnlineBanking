7f75ceb325710fc84338b4005914546ac78cf879
Syr7-s <isasayar7@github.com>
2021-03-16 15:48:31 +0300

    attributes were added to the Transfer

--- account-service/src/main/java/com/syrisa/onlinebank/microservice/accountservice/entity/impl/Transfer.java
+++ account-service/src/main/java/com/syrisa/onlinebank/microservice/accountservice/entity/impl/Transfer.java
@@ -1,4 +1,26 @@
 package com.syrisa.onlinebank.microservice.accountservice.entity.impl;
 
+import lombok.*;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.MongoId;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Document
 public class Transfer {
+    @MongoId
+    private long fromAccountIban;
+    private long toAccountIban;
+    private int depositMoney;
+    private int receiveMoney;
+    private String processType;
+    private LocalTime time;
+    private LocalDate date;
 }
