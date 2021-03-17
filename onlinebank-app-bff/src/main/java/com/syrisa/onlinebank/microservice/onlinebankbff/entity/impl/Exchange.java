94f9692122406bbe0628490b9060bd46ab24134e
Syr7-s <isasayar7@github.com>
2021-03-17 16:27:56 +0300

    lombok annotations were added.Also attributes were added.

--- onlinebank-app-bff/src/main/java/com/syrisa/onlinebank/microservice/onlinebankbff/entity/impl/Exchange.java
+++ onlinebank-app-bff/src/main/java/com/syrisa/onlinebank/microservice/onlinebankbff/entity/impl/Exchange.java
@@ -1,4 +1,24 @@
 package com.syrisa.onlinebank.microservice.onlinebankbff.entity.impl;
 
+import lombok.*;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.UUID;
+
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Exchange {
+    private UUID processNumber;
+    private String fromAccountIban;
+    private String toAccountIban;
+    private int depositMoney;
+    private int receiveMoney;
+    private String processType;
+    private LocalTime time;
+    private LocalDate date;
 }
