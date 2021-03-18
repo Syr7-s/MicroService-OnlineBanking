bdd7ef519213b6bf3ee7f2b19e907b81aa1509c2
Syr7-s <isasayar7@github.com>
2021-03-18 21:58:46 +0300

    CrudRepository was added

--- card-service/src/main/java/com/onlinebank/microservice/cardservice/repository/CreditCardRepository.java
+++ card-service/src/main/java/com/onlinebank/microservice/cardservice/repository/CreditCardRepository.java
@@ -1,7 +1,9 @@
 package com.onlinebank.microservice.cardservice.repository;
 
+import com.onlinebank.microservice.cardservice.entity.impl.CreditCard;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CreditCardRepository {
+public interface CreditCardRepository extends CrudRepository<CreditCard,Long> {
 }
