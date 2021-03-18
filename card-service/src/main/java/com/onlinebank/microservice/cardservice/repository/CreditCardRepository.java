d0783eebf02886421bd499f6855c3eddbad13d67
Syr7-s <isasayar7@github.com>
2021-03-18 22:17:50 +0300

    CreditCardRepository was edited.

--- card-service/src/main/java/com/onlinebank/microservice/cardservice/repository/CreditCardRepository.java
+++ card-service/src/main/java/com/onlinebank/microservice/cardservice/repository/CreditCardRepository.java
@@ -1,6 +1,5 @@
 package com.onlinebank.microservice.cardservice.repository;
 
-import com.onlinebank.microservice.cardservice.entity.impl.BankCard;
 import com.onlinebank.microservice.cardservice.entity.impl.CreditCard;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
