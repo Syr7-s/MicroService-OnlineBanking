f1067bc25f2a38dacfb4cada85820df8d386bc9d
Syr7-s <isasayar7@github.com>
2021-03-18 22:17:08 +0300

    findAll method was added.

--- card-service/src/main/java/com/onlinebank/microservice/cardservice/repository/CreditCardRepository.java
+++ card-service/src/main/java/com/onlinebank/microservice/cardservice/repository/CreditCardRepository.java
@@ -1,9 +1,13 @@
 package com.onlinebank.microservice.cardservice.repository;
 
+import com.onlinebank.microservice.cardservice.entity.impl.BankCard;
 import com.onlinebank.microservice.cardservice.entity.impl.CreditCard;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CreditCardRepository extends CrudRepository<CreditCard,Long> {
+    Page<CreditCard> findAll(Pageable pageable);
 }
