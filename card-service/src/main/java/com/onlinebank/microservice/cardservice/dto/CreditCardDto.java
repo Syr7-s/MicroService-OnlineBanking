2ffceabc8d824f780dcdfc0796df45ee3ae8ec14
Syr7-s <isasayar7@github.com>
2021-03-18 21:52:50 +0300

    attributes were added to CreditCardDto

--- card-service/src/main/java/com/onlinebank/microservice/cardservice/dto/CreditCardDto.java
+++ card-service/src/main/java/com/onlinebank/microservice/cardservice/dto/CreditCardDto.java
@@ -1,4 +1,23 @@
 package com.onlinebank.microservice.cardservice.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.onlinebank.microservice.cardservice.utility.currency.Currency;
+import lombok.Builder;
+import lombok.Data;
+
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import java.time.LocalDate;
+
+@Data
+@Builder
 public class CreditCardDto {
+    private long cardAccountNumber;
+    private String cardNameSurname;
+    private int cardPassword;
+    private LocalDate expirationDate;
+    private String securityCode;
+    private int cardLimit;
+    private int cardDebt;
+    private Currency currency;
 }
