26c911f94832e07a66da6d190a5f0e05ee27f1ff
Syr7-s <isasayar7@github.com>
2021-03-18 22:05:56 +0300

    BankCardService implemented to the BankCardServiceImpl

--- card-service/src/main/java/com/onlinebank/microservice/cardservice/service/concrete/BankCardServiceImpl.java
+++ card-service/src/main/java/com/onlinebank/microservice/cardservice/service/concrete/BankCardServiceImpl.java
@@ -1,7 +1,35 @@
 package com.onlinebank.microservice.cardservice.service.concrete;
 
+import com.onlinebank.microservice.cardservice.entity.impl.BankCard;
+import com.onlinebank.microservice.cardservice.service.abstrct.BankCardService;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 @Service
-public class BankCardServiceImpl {
+public class BankCardServiceImpl implements BankCardService {
+    @Override
+    public BankCard create(BankCard bankCard) {
+        return null;
+    }
+
+    @Override
+    public BankCard update(BankCard bankCard) {
+        return null;
+    }
+
+    @Override
+    public BankCard findCard(long cardNumber) {
+        return null;
+    }
+
+    @Override
+    public String delete(long cardNumber) {
+        return null;
+    }
+
+    @Override
+    public Page<BankCard> cardPage(Pageable pageable) {
+        return null;
+    }
 }
