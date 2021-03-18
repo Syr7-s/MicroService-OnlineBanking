79b4b8487050147e351c4114a4ed84e853deffbc
Syr7-s <isasayar7@github.com>
2021-03-18 22:18:52 +0300

    @Service and @Transactional annotations were added.

--- card-service/src/main/java/com/onlinebank/microservice/cardservice/service/concrete/CreditCardServiceImpl.java
+++ card-service/src/main/java/com/onlinebank/microservice/cardservice/service/concrete/CreditCardServiceImpl.java
@@ -1,4 +1,9 @@
 package com.onlinebank.microservice.cardservice.service.concrete;
 
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
 public class CreditCardServiceImpl {
 }
