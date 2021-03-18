200a2bed715a2081aae7ca3853008a0e5887fba0
Syr7-s <isasayar7@github.com>
2021-03-18 22:01:51 +0300

    Card Service was created.Also five abstract methods were added.

---
+++ card-service/src/main/java/com/onlinebank/microservice/cardservice/service/abstrct/CardService.java
@@ -0,0 +1,18 @@
+package com.onlinebank.microservice.cardservice.service.abstrct;
+
+import com.onlinebank.microservice.cardservice.entity.Card;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+public interface CardService<T extends Card> {
+    T create(T t);
+
+    T update(T t);
+
+    T findCard(long cardNumber);
+
+    String delete(long cardNumber);
+
+    Page<T> cardPage(Pageable pageable);
+
+}
