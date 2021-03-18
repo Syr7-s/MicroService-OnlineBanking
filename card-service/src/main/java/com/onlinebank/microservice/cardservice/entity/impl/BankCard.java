110d19082e94f08748dcc896b4a4804df352799f
Syr7-s <isasayar7@github.com>
2021-03-18 21:50:50 +0300

    toBankCardDto method was created.

--- card-service/src/main/java/com/onlinebank/microservice/cardservice/entity/impl/BankCard.java
+++ card-service/src/main/java/com/onlinebank/microservice/cardservice/entity/impl/BankCard.java
@@ -1,6 +1,7 @@
 package com.onlinebank.microservice.cardservice.entity.impl;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.onlinebank.microservice.cardservice.dto.BankCardDto;
 import com.onlinebank.microservice.cardservice.entity.Card;
 import lombok.*;
 
@@ -21,4 +22,14 @@ public class BankCard implements Card {
     @JsonFormat(pattern = "yyyy-MM-dd")
     private LocalDate bankCardExpirationDate;
     private String bankCardSecurityCode;
+
+    public BankCardDto toBankCardDto(){
+        return BankCardDto.builder()
+                .bankCardAccountNumber(this.bankCardAccountNumber)
+                .bankCardNameSurname(this.bankCardNameSurname)
+                .bankCardPassword(this.bankCardPassword)
+                .bankCardExpirationDate(this.bankCardExpirationDate)
+                .bankCardSecurityCode(this.bankCardSecurityCode)
+                .build();
+    }
 }
