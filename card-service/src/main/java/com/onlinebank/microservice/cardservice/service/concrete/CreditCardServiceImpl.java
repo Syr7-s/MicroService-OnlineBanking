562ead392d6d8a8772701916a84e630b12db2a4b
Syr7-s <isasayar7@github.com>
2021-03-18 22:19:41 +0300

    CreditCardServiceImpl implemetented to the CreditCardServiceImpl

--- card-service/src/main/java/com/onlinebank/microservice/cardservice/service/concrete/CreditCardServiceImpl.java
+++ card-service/src/main/java/com/onlinebank/microservice/cardservice/service/concrete/CreditCardServiceImpl.java
@@ -1,9 +1,37 @@
 package com.onlinebank.microservice.cardservice.service.concrete;
 
+import com.onlinebank.microservice.cardservice.entity.impl.CreditCard;
+import com.onlinebank.microservice.cardservice.service.abstrct.CreditCardService;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @Transactional
-public class CreditCardServiceImpl {
+public class CreditCardServiceImpl implements CreditCardService {
+    @Override
+    public CreditCard create(CreditCard creditCard) {
+        return null;
+    }
+
+    @Override
+    public CreditCard update(CreditCard creditCard) {
+        return null;
+    }
+
+    @Override
+    public CreditCard findCard(long cardNumber) {
+        return null;
+    }
+
+    @Override
+    public String delete(long cardNumber) {
+        return null;
+    }
+
+    @Override
+    public Page<CreditCard> cardPage(Pageable pageable) {
+        return null;
+    }
 }
