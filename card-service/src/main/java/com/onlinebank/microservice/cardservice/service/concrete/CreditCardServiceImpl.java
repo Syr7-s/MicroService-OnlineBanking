3df0961069590ae0a392d7a708bb2b974b4dc7d0
Syr7-s <isasayar7@github.com>
2021-03-18 22:24:05 +0300

    CreditCardServiceImpl was edited.

--- card-service/src/main/java/com/onlinebank/microservice/cardservice/service/concrete/CreditCardServiceImpl.java
+++ card-service/src/main/java/com/onlinebank/microservice/cardservice/service/concrete/CreditCardServiceImpl.java
@@ -1,37 +1,57 @@
 package com.onlinebank.microservice.cardservice.service.concrete;
 
 import com.onlinebank.microservice.cardservice.entity.impl.CreditCard;
+import com.onlinebank.microservice.cardservice.repository.CreditCardRepository;
 import com.onlinebank.microservice.cardservice.service.abstrct.CreditCardService;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.server.ResponseStatusException;
 
 @Service
 @Transactional
 public class CreditCardServiceImpl implements CreditCardService {
+    private final CreditCardRepository creditCardRepository;
+
+    public CreditCardServiceImpl(CreditCardRepository creditCardRepository) {
+        this.creditCardRepository = creditCardRepository;
+    }
+
     @Override
     public CreditCard create(CreditCard creditCard) {
-        return null;
+        return creditCardRepository.save(creditCard);
     }
 
     @Override
     public CreditCard update(CreditCard creditCard) {
-        return null;
+        try {
+            if (findCard(creditCard.getCardAccountNumber()) != null) {
+                return creditCardRepository.save(creditCard);
+            } else {
+                throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Card account not found.");
+            }
+        } catch (Exception exception) {
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, exception.getMessage());
+        }
     }
 
     @Override
     public CreditCard findCard(long cardNumber) {
-        return null;
+        return creditCardRepository.findById(cardNumber)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Card not found."));
     }
 
     @Override
     public String delete(long cardNumber) {
-        return null;
+        CreditCard creditCard = findCard(cardNumber);
+        creditCardRepository.delete(creditCard);
+        return cardNumber + " number card was deleted.";
     }
 
     @Override
     public Page<CreditCard> cardPage(Pageable pageable) {
-        return null;
+        return creditCardRepository.findAll(pageable);
     }
 }
