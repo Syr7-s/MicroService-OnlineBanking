ac72fbe2e0412899e207c551f9beffc6bbe2aba3
Syr7-s <isasayar7@github.com>
2021-03-20 21:36:03 +0300

    @RestController and @RequestMapping annotations were added.

--- card-service/src/main/java/com/onlinebank/microservice/cardservice/controller/BankCardController.java
+++ card-service/src/main/java/com/onlinebank/microservice/cardservice/controller/BankCardController.java
@@ -1,4 +1,10 @@
 package com.onlinebank.microservice.cardservice.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/bankcard")
 public class BankCardController {
+
 }
